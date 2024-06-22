dd28ac8cb65e4c9409aac6c09aba646bf81277ce
Rmurthy1 <rahul.a.murthy@gmail.com>
2024-06-21 17:20:31 -0700

    added lcd screen and temperature probe
    still need to pass the data to the wifi board

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-#include "ph_surveyor.h"             
-
+#include "ph_surveyor.h"
+// lcd libraries
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+// temp sensor libraries
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 const byte rxPin = 2;
 const byte txPin = 3;
@@ -16,7 +21,21 @@ SoftwareSerial mySerial (rxPin, txPin);
 String message = "";
 bool messageReady = false;
 
+// set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+const int ONE_WIRE_BUS = 6;
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+
 int distanceFromSensor();
+void lcdSetup();
+void updateLCD(int distance, double temperature);
 
 void setup() {
   Serial.begin(9600);
@@ -31,15 +50,20 @@ void setup() {
     
   // Set the baud rate for the SoftwareSerial object
   mySerial.begin(9600);
+
+  lcdSetup();
+  sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
+
+}
+
+void lcdSetup() {
+  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
 }
 
 void loop() {
-  // if there is data available from the software serial port
-  /*while (Serial.available()) { 
-    message = Serial.readString();
-    messageReady = true;
-    Serial.println("wrote");
-  }*/
   
   // if there is a message to send
   if (messageReady == true) {
@@ -51,11 +75,16 @@ void loop() {
   // every 500ms, read the distance from the sensor
   if (millis() % 5000 == 0) {
     int distance = distanceFromSensor();
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    double temperature = sensors.getTempFByIndex(0);
+    updateLCD(distance, temperature);
     Serial.println(distance);
     message = String(distance);
     messageReady = true;
   }
 }
+
+
 
 // function to read the distance from the ultrasonic sensor
 int distanceFromSensor() {
@@ -69,3 +98,13 @@ int distanceFromSensor() {
   return distance;
 }
 
+void updateLCD(int distance, double temperature) {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Distance: ");
+  lcd.print(distance);
+  lcd.print(" cm");
+  lcd.setCursor(0,1);
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+}
