39d7fb4e9aa8f7e22d9fe36550d624d44841116f
Rmurthy1 <rahul.a.murthy@gmail.com>
2024-07-20 06:46:58 -0700

    added the probe

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+// ph probe library
 #include "ph_surveyor.h"
 // lcd libraries
 #include <Wire.h> 
@@ -27,10 +28,16 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 const int ONE_WIRE_BUS = 6;
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire(ONE_WIRE_BUS); // dont forget the pullup resistor (3.3v to data via 4.7k Ohm resistor)
 
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
+
+// Number of temperature devices found
+int numberOfDevices;
+
+// We'll use this variable to store a found device address
+DeviceAddress tempDeviceAddress;
 
 
 Surveyor_pH pH = Surveyor_pH(A0);   
@@ -44,8 +51,9 @@ char user_data[bufferlen];
 
 int distanceFromSensor();
 void lcdSetup();
-void updateLCD(int distance, double temperature);
+void updateLCD(float ph);
 void parse_cmd(char* string);
+float getTempForIndex(int index);
 
 void setup() {
   Serial.begin(9600);
@@ -64,9 +72,12 @@ void setup() {
   lcdSetup();
   sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
 
+  // Grab a count of devices on the wire
+  numberOfDevices = sensors.getDeviceCount();
+
   delay(200);
-  Serial.println(F("Use commands \"CAL,7\", \"CAL,4\", and \"CAL,10\" to calibrate the circuit to those respective values"));
-  Serial.println(F("Use command \"CAL,CLEAR\" to clear the calibration"));
+  Serial.println(F("Use commands \"7\", \"4\", and \"10\" to calibrate the circuit to those respective values"));
+  Serial.println(F("Use command \"CAL,CLEAR\" to clear the calibration (DOESNT WORK YET)"));
   if (pH.begin()) {                                     
     Serial.println("Loaded EEPROM");
   }
@@ -80,7 +91,28 @@ void lcdSetup() {
   lcd.backlight();
 }
 
+double temp1;
+double temp2;
+double temp3;
+int distance;
+int ph;
+
+// function to get the temperature for a given index
+float getTempForIndex(int index) {
+  sensors.getAddress(tempDeviceAddress, index);
+  return sensors.getTempF(tempDeviceAddress);
+}
+
+// function to update all the temperatures
+void updateAllTemps() {
+  sensors.requestTemperatures();  // Send the command to get temperatures
+  temp1 = getTempForIndex(0);
+  temp2 = getTempForIndex(1);
+  temp3 = getTempForIndex(2);
+}
+
 void loop() {
+
   
   // if there is a message to send
   if (messageReady == true) {
@@ -88,7 +120,7 @@ void loop() {
     mySerial.write(message.c_str());
     Serial.println("sent");
   }
-
+  /*
   if (Serial.available() > 0) {                                                      
     user_bytes_received = Serial.readBytesUntil(13, user_data, sizeof(user_data));   
   }
@@ -97,22 +129,34 @@ void loop() {
     parse_cmd(user_data);                                                          
     user_bytes_received = 0;                                                        
     memset(user_data, 0, sizeof(user_data));                                         
-  }
+  }*/
 
+ /*
+  if (millis() % 500 == 0) {
+    distance = distanceFromSensor();
+    sensors.requestTemperatures();  // Send the command to get temperatures
+    temp1 = getTempForIndex(0);
+    temp2 = getTempForIndex(1);
+    temp3 = getTempForIndex(2);
+    updateLCD(distance, temp1);
+  }
+*/
   // every 500ms, read the distance from the sensor
-  if (millis() % 5000 == 0) {
-    int distance = distanceFromSensor();
-    sensors.requestTemperatures(); // Send the command to get temperatures
-    double temperature = sensors.getTempFByIndex(0);
-    updateLCD(distance, temperature);
-    Serial.println(distance);
-    message = String(distance);
+  if (millis() % 1000 == 0) {
+    Serial.println("Reading pH");
+    distance = distanceFromSensor();
+    sensors.requestTemperatures();  // Send the command to get temperatures
+    temp1 = getTempForIndex(0);
+    temp2 = getTempForIndex(1);
+    temp3 = getTempForIndex(2);
+    float ph = pH.read_ph();
+    updateLCD(ph);
+    String data = "5;" + String(distance) + ";" + String(temp1) + ";" + String(temp2) + ";" + String(temp3) + ";" + String(ph);
+    Serial.println(data);
+    message = data;
     messageReady = true;
-    Serial.println(pH.read_ph());  
   }
 }
-
-
 
 // function to read the distance from the ultrasonic sensor
 int distanceFromSensor() {
@@ -126,28 +170,34 @@ int distanceFromSensor() {
   return distance;
 }
 
-void updateLCD(int distance, double temperature) {
+void updateLCD(float ph) {
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Distance: ");
-  lcd.print(distance);
-  lcd.print(" cm");
+  lcd.print("0:");
+  lcd.print(temp1);
+  lcd.print(" ");
+  lcd.print("1:");
+  lcd.print(temp2);
   lcd.setCursor(0,1);
-  lcd.print("Temp: ");
-  lcd.print(temperature);
+  lcd.print("2:");
+  lcd.print(temp3);
+  lcd.print(" ");
+  lcd.print("P:");
+  lcd.print(ph);
 }
 
 void parse_cmd(char* string) {                   
-  strupr(string);                                
-  if (strcmp(string, "CAL,7") == 0) {       
+  strupr(string);
+  int calNum = String(string).toInt();                                
+  if (calNum == 7) {       
     pH.cal_mid();                                
     Serial.println("MID CALIBRATED");
   }
-  else if (strcmp(string, "CAL,4") == 0) {            
+  else if (calNum == 4) {            
     pH.cal_low();                                
     Serial.println("LOW CALIBRATED");
   }
-  else if (strcmp(string, "CAL,10") == 0) {      
+  else if (calNum == 10) {      
     pH.cal_high();                               
     Serial.println("HIGH CALIBRATED");
   }
